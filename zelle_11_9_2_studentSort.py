3f686250422eeb0759bb961f50a3d9cdf1b93335
psarran <psarran@gmail.com>
2017-10-02 22:13:58 -0500

    Add files via upload

--- zelle_11_9_2_studentSort.py
+++ zelle_11_9_2_studentSort.py
@@ -1,4 +1,4 @@
-#zelle 11.9.2 studetSort
+#zelle 11.9.2 studentSort
 
 """ Program to import student grade data, sort on GPA, name, or # of credits,
     then output the file. Default sort key is name.
@@ -7,8 +7,8 @@
 from zelle_10_4_text_exercise_student_grades import Student, makeStudent
 
 def getInputs():
-    """ gather the input and output file names and sort key.
-        also sets default file names if none are entered
+    """ gather the input and output file names, sort key and sort direction.
+        also sets defaults if info isn't entered
     """
     infilename = input('name of file w student data: ')
     if infilename == "":
@@ -17,8 +17,10 @@ def getInputs():
     if outfilename == "":
         outfilename = 'zelle 11.9.2 student sort.txt'
     sortkey = input("sort key - 'gpa' 'name' or 'credits': ")
-    outfile = open(outfilename, 'w')
-    return infilename, outfilename, sortkey
+    direction = input("sort ascending ('a') or descending ('d'): ")
+    if direction == "descending":
+        direction = 'd'
+    return infilename, outfilename, sortkey, direction
 
 def readStudents(infilename):
     """ given the file name, import the data and create a list of student objects
@@ -40,9 +42,10 @@ def writeStudents(students, outfilename):
             file=outfile)
     outfile.close()
 
-def sortStudents(students, sortkey):
-    """ sort the list of student objects on the given key
+def sortStudents(students, sortkey, direction):
+    """ sort the list of student objects on the given key and direction
         default sort is name if none or invalid key is given
+        default sort direction is ascending
     """
     if sortkey == 'gpa':
         students.sort(key=Student.gpa)
@@ -50,12 +53,15 @@ def sortStudents(students, sortkey):
         students.sort(key=Student.getQPoints)
     else:
         students.sort(key=Student.getName)
+
+    if direction == 'd':
+        students.reverse()
     return students
 
 def main():
-    infilename, outfilename, sortkey = getInputs()
+    infilename, outfilename, sortkey, direction = getInputs()
     students = readStudents(infilename)
-    students = sortStudents(students, sortkey)
+    students = sortStudents(students, sortkey, direction)
     writeStudents(students, outfilename)
     
 if __name__ == '__main__': main()
